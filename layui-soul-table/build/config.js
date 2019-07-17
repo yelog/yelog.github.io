f021b39d952dd9be0194482f2cc68cffea79c9bf
yelog <jaytp@qq.com>
2019-07-18 05:25:59 +0800

    Site updateed: 2019-07-18 05:25:47

---
+++ layui-soul-table/build/config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+exports.alias = {
+  documents: path.resolve(__dirname, '../documents')
+};
+
+exports.vue = {
+  root: 'Vue',
+  commonjs: 'vue',
+  commonjs2: 'vue',
+  amd: 'vue'
+};
+
+exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date\.js/;
