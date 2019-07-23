10f6ee267cf4e030bad5864d00ec29ea5c51854f
yelog <jaytp@qq.com>
2019-07-23 22:25:19 +0800

    Site updateed: 2019-07-23 22:25:17

--- layui-soul-table/ext/tableFilter.js
+++ layui-soul-table/ext/tableFilter.js
@@ -2465,7 +2465,19 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
                 ,anim: -1
                 ,fixed: false
             });
-            var columns = [].concat.apply([], myTable.cols),
+            var cols = this.deepParse(this.deepStringify(myTable.cols))
+                ,style = myTable.elem.next().find('style')[0]
+                ,sheet = style.sheet || style.styleSheet || {}
+                ,rules = sheet.cssRules || sheet.rules;
+
+            layui.each(rules, function (i, item) {
+                if (item.style.width) {
+                    var keys = item.selectorText.split('-');
+                    cols[keys[3]][keys[4]]['width'] = parseInt(item.style.width)
+                }
+            })
+
+            var columns = [].concat.apply([], cols),
                 data = JSON.parse(JSON.stringify(myTable.data || layui.table.cache[myTable.id])),
                 title = {},
                 showField = {},
@@ -2520,7 +2532,7 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
             }
 
             // 制定显示列和顺序
-            var index = 0;
+            var index = 0, alignTrans = {'left':'top', 'center':'center', 'right': 'bottom'}, borderTypes=['top','bottom', 'left', 'right'];
             for (var i = 0; i < columns.length; i++) {
                 if ((columns[i].field || columns[i].type === 'numbers') && !columns[i].hide) {
                     columnsMap[columns[i].type === 'numbers' ? 'LAY_TABLE_INDEX' : columns[i].field] = columns[i];
@@ -2529,7 +2541,25 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
                     }
                     title[columns[i].type === 'numbers' ? 'LAY_TABLE_INDEX' : columns[i].field] = columns[i].title;
                     showField[columns[i].type === 'numbers' ? 'LAY_TABLE_INDEX' : columns[i].field] = function (field, line, data, curIndex) {
-                        var bgColor = 'ffffff', color = '000000', family = 'Calibri', size = 12, cellType = 's';
+                        var bgColor = 'ffffff', color = '000000', family = 'Calibri', size = 12, cellType = 's',
+                            border = {
+                                top: {
+                                    style: 'thin',
+                                    color: {indexed: 64}
+                                },
+                                bottom: {
+                                    style: 'thin',
+                                    color: {indexed: 64}
+                                },
+                                left: {
+                                    style: 'thin',
+                                    color: {indexed: 64}
+                                },
+                                right: {
+                                    style: 'thin',
+                                    color: {indexed: 64}
+                                }
+                            }
                         if (curIndex === 0) {
                             bgColor = 'C7C7C7';
                             if (mainExcel.head) {
@@ -2544,6 +2574,14 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
                                 family = curExcel.head.family || family;
                                 size = curExcel.head.size || size;
                             }
+                            if (mainExcel.border) {
+                                for (var j = 0; j < borderTypes.length; j++) {
+                                    if (mainExcel.border[borderTypes[j]]) {
+                                        border[borderTypes[j]].style = mainExcel.border[borderTypes[j]].style || border[borderTypes[j]].style
+                                        border[borderTypes[j]].color = mainExcel.border[borderTypes[j]].color || border[borderTypes[j]].color
+                                    }
+                                }
+                            }
                         } else {
                             if (mainExcel.font) {
                                 bgColor = mainExcel.font.bgColor || bgColor;
@@ -2557,6 +2595,14 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
                                 family = curExcel.font.family || family;
                                 size = curExcel.head.size || size;
                             }
+                            if (curExcel.border) {
+                                for (var j = 0; j < borderTypes.length; j++) {
+                                    if (curExcel.border[borderTypes[j]]) {
+                                        border[borderTypes[j]].style = curExcel.border[borderTypes[j]].style || border[borderTypes[j]].style
+                                        border[borderTypes[j]].color = curExcel.border[borderTypes[j]].color || border[borderTypes[j]].color
+                                    }
+                                }
+                            }
                             if (columnsMap[field].excel) {
                                 var colExcel = typeof columnsMap[field].excel == 'function' ? columnsMap[field].excel.call(this, line) : columnsMap[field].excel
                                 if (colExcel) {
@@ -2565,6 +2611,15 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
                                     family = colExcel.family || family;
                                     size = colExcel.size || size;
                                     cellType = colExcel.cellType || cellType;
+
+                                    if (colExcel.border) {
+                                        for (var j = 0; j < borderTypes.length; j++) {
+                                            if (colExcel.border[borderTypes[j]]) {
+                                                border[borderTypes[j]].style = colExcel.border[borderTypes[j]].style || border[borderTypes[j]].style
+                                                border[borderTypes[j]].color = colExcel.border[borderTypes[j]].color || border[borderTypes[j]].color
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -2577,31 +2632,14 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
                                 : line[field] || '',// v 代表单元格的值
                             s: {// s 代表样式
                                 alignment: {
-                                    horizontal: 'center',
+                                    horizontal: columnsMap[field].align ? alignTrans[columnsMap[field].align] : 'top',
                                     vertical: 'center'
                                 },
                                 font: {name: family, sz: size, color: {rgb: color}},
                                 fill: {
                                     fgColor: {rgb: bgColor, bgColor: {indexed: 64}}
                                 },
-                                border: {
-                                    top: {
-                                        style: 'thin',
-                                        color: {indexed: 64}
-                                    },
-                                    bottom: {
-                                        style: 'thin',
-                                        color: {indexed: 64}
-                                    },
-                                    left: {
-                                        style: 'thin',
-                                        color: {indexed: 64}
-                                    },
-                                    right: {
-                                        style: 'thin',
-                                        color: {indexed: 64}
-                                    },
-                                }
+                                border: border
                             },
                             t: cellType
                         };
@@ -2632,9 +2670,22 @@ layui.define(['table', 'form', 'laydate', 'util', 'excel'], function (exports) {
                 }
                 return value;
             });
-        }
+        },
+        deepParse: function (str) {
+            var JSON_SERIALIZE_FIX = {
+                PREFIX : "[[JSON_FUN_PREFIX_",
+                SUFFIX : "_JSON_FUN_SUFFIX]]"
+            };
+            return JSON.parse(str,function(key, value){
+                if(typeof value === 'string' &&
+                    value.indexOf(JSON_SERIALIZE_FIX.SUFFIX)>0 && value.indexOf(JSON_SERIALIZE_FIX.PREFIX)==0){
+                    return eval("("+value.replace(JSON_SERIALIZE_FIX.PREFIX,"").replace(JSON_SERIALIZE_FIX.SUFFIX,"")+")");
+                }
+                return value;
+            })||{};
+        },
         /* layui table 中原生的方法 */
-        ,getScrollWidth(elem) {
+        getScrollWidth: function (elem) {
             var width = 0;
             if (elem) {
                 width = elem.offsetWidth - elem.clientWidth;
